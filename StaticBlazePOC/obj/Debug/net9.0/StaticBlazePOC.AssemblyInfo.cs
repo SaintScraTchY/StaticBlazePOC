e98c3b3f87cd6d865aa288acfb68a1b1393d4a7d
SaintScraTchY <mehrshad2028@gmail.com>
2025-02-27 14:49:56 +0330

    Add GitHub repository and account input fields to upload forms

--- StaticBlazePOC/obj/Debug/net9.0/StaticBlazePOC.AssemblyInfo.cs
+++ StaticBlazePOC/obj/Debug/net9.0/StaticBlazePOC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaticBlazePOC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97c272784112733d6d42673c8bf9ebe9b2c05ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43ba1a73a5148439ed3a878c2e54e4e6a0155b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaticBlazePOC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaticBlazePOC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
